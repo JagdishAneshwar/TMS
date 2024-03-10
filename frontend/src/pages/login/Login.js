789c0a2ea3835cfcf95f2538766c655da1c2eb6a
Jagdish Aneshwar <aneshwarjagdish@gmail.com>
2024-03-10 15:16:30 +0530

    Update Login.js

--- frontend/src/pages/login/Login.js
+++ frontend/src/pages/login/Login.js
@@ -15,7 +15,7 @@ const Login = () => {
 
   const onClickLogin = async (e) => {
     e.preventDefault();
-    const res = await fetch("http://localhost:5000/api/auth/login", {
+    const res = await fetch("https://api-tms.vercel.app/api/auth/login", {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({ email, password }),
